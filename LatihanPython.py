f9920db9a9815fcc2aee32ffc746ea8b63037727
srezaputra14@gmail.com <srezaputra14@gmail.com>
2021-10-19 19:21:50 +0700

    menambahkan file LatihanPython.py

---
+++ LatihanPython.py
@@ -0,0 +1,15 @@
+# mengambil input
+Nama = input("Siapa nama lengkap kamu:")
+Namapanggilan = input("Nama Panggilan kamu:")
+NPM = input("NPM atau NIM kamu:")
+tempatlahir = input("tempat lahir kamu:")
+umur = input("umur kamu:")
+alamat = input("alamat rumah kamu:")
+telepon = input("No telepon kamu:")
+
+# menampilkan output
+print("Assalamualaikum")
+
+print("Let me introduce myself. My name is",Nama,"but you can call me",Namapanggilan,". My NPM is",NPM,". I was born in",tempatlahir,"and i am ",umur,"years old. I am very glad if you want to come to my house in",alamat,"so, dont forget to call me before with the number",telepon,)
+
+print("Thank you")
